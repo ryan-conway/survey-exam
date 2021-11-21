3129d7ea60bfb501e410cca96491538369948d4e
Ryan Conway <rjmconway@outlook.com>
2021-11-21 17:19:55 +0800

    feat: add dependency module for surveys

---
+++ app/src/main/java/com/example/nimblesurveys/di/SurveyModule.kt
@@ -0,0 +1,37 @@
+package com.example.nimblesurveys.di
+
+import com.example.nimblesurveys.data.adapter.SurveyAdapter
+import com.example.nimblesurveys.data.api.survey.SurveyApiService
+import com.example.nimblesurveys.data.repository.SurveyRepositoryImpl
+import com.example.nimblesurveys.domain.repository.SurveyRepository
+import com.example.nimblesurveys.domain.usecase.GetSurveysUseCase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object SurveyModule {
+
+    @Provides
+    @Singleton
+    fun provideSurveyRepository(
+        retrofit: Retrofit
+    ): SurveyRepository {
+        return SurveyRepositoryImpl(
+            retrofit.create(SurveyApiService::class.java),
+            SurveyAdapter()
+        )
+    }
+}
+
+@Module
+@InstallIn(SingletonComponent::class)
+object SurveyUseCaseModule {
+
+    @Provides
+    fun provideGetSurveysUseCase(repository: SurveyRepository) = GetSurveysUseCase(repository)
+}
