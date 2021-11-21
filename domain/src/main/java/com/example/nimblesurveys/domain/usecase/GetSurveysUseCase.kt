44ba32937a831e8412d13b084813f0e32bb0032b
Ryan Conway <rjmconway@outlook.com>
2021-11-21 17:11:22 +0800

    fix: add token param to GetSurveysUseCase

--- domain/src/main/java/com/example/nimblesurveys/domain/usecase/GetSurveysUseCase.kt
+++ domain/src/main/java/com/example/nimblesurveys/domain/usecase/GetSurveysUseCase.kt
@@ -2,13 +2,14 @@ package com.example.nimblesurveys.domain.usecase
 
 import com.example.nimblesurveys.domain.model.Result
 import com.example.nimblesurveys.domain.model.Survey
+import com.example.nimblesurveys.domain.model.Token
 import com.example.nimblesurveys.domain.repository.SurveyRepository
 
 class GetSurveysUseCase(private val repository: SurveyRepository) {
 
-    suspend fun execute(): Result<List<Survey>> {
+    suspend fun execute(token: Token): Result<List<Survey>> {
         return try {
-            val surveys = repository.getSurveys()
+            val surveys = repository.getSurveys(token)
             Result.Success(surveys)
         } catch (e: Throwable) {
             Result.Failure(e)
