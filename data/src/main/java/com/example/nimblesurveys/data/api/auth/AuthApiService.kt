a21d0b5042a8c8442fc9476bcd9c8370fc51f89e
Ryan Conway <rjmconway@outlook.com>
2021-11-25 19:30:47 +0800

    feat: create AuthInterceptor to add access token to requests

--- data/src/main/java/com/example/nimblesurveys/data/api/auth/AuthApiService.kt
+++ data/src/main/java/com/example/nimblesurveys/data/api/auth/AuthApiService.kt
@@ -3,17 +3,19 @@ package com.example.nimblesurveys.data.api.auth
 import com.example.nimblesurveys.data.api.response.ApiResponse
 import retrofit2.http.Body
 import retrofit2.http.GET
-import retrofit2.http.Header
+import retrofit2.http.Headers
 import retrofit2.http.POST
 
 interface AuthApiService {
 
     @POST("api/v1/oauth/token")
+    @Headers("No-Authentication: true")
     suspend fun signIn(
         @Body signInRequest: SignInRequest
     ): ApiResponse<SignInAttributes>
 
     @POST("api/v1/oauth/token")
+    @Headers("No-Authentication: true")
     suspend fun getAccessToken(
         @Body accessTokenRequest: AccessTokenRequest
     ): ApiResponse<AccessTokenAttributes>
